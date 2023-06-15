9367bf61e594234832fc5c882ee612c2535d960f
Shubham <shivam306joshi@gmail.com>
2023-06-15 17:04:27 +0530

    third commit

--- src/components/Array/homeArray.js
+++ src/components/Array/homeArray.js
@@ -1,11 +1,10 @@
 import React from 'react'
-import Link from '@material-ui/core/Link'
 
 const HomeArray = () => {
     return (
         <header className="App-header">
             <span>JavaScript Array Methods</span><br />
-            <Link href="/arrayMethods"><button className='goThroughButton'>GO THROUGH..</button></Link>
+            <a href="/arrayMethods"><button className='goThroughButton'>GO THROUGH..</button></a>
         </header>
     )
 }
